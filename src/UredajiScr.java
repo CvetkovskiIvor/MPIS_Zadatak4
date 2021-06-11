9c11d2891fa7740444a6a059f53913d47f0b18f6
ivor <cvetkovskiivor@gmail.com>
2021-06-11 16:47:18 +0200

    Nadopunjena klasa UredajiScr

--- src/UredajiScr.java
+++ src/UredajiScr.java
@@ -1,24 +1,133 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class UredajiScr {
+public class UredajiScr extends StartScr implements ActionListener{
 	
 	public void uredajiScr() {
 		
 	}
 	
 	
-	public void promijeniStanjePrekidac() {
+	public void actionPerformed(ActionEvent e) {
+		if(e.getActionCommand() == "") {
+			
+			prekidacSP.uklop(prekidacSP);
+			
+		}
 		
-	}
-	
-	public void promijeniStanjeSabirnickiRastavljac() {
+		if(e.getActionCommand() == "") {
+			
+			prekidacSP.isklop(prekidacSP);
+			
+		}
+
+		if(e.getActionCommand() == "") {
+			
+			prekidacDP.uklop(prekidacDP);
+			
+		}
 		
-	}
+		if(e.getActionCommand() == "") {
+			
+			prekidacDP.isklop(prekidacDP);
+			
+		}
+
+		if(e.getActionCommand() == "") {
 	
-	public void promijeniStanjeIzlazniRastavljac() {
+			izlazniRastavljacDP.uklop(izlazniRastavljacDP);
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			izlazniRastavljacDP.isklop(izlazniRastavljacDP);
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			izlazniRastavljacSP.uklop(izlazniRastavljacSP);
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			izlazniRastavljacSP.isklop(izlazniRastavljacSP);
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			//rastavljacUzemljenjaDP
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			//rastavljacUzemljenjaDP
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			//rastavljacUzemljenjaSP
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			//rastavljacUzemljenjaSP
+	
+		}
 		
-	}
+		if(e.getActionCommand() == "") {
+			
+			sabirnickiRastavljacDPS1.uklop(sabirnickiRastavljacDPS1);
 	
-	public void promijeniStanjeRastavljacUzemljenja() {
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			sabirnickiRastavljacDPS1.isklop(sabirnickiRastavljacDPS1);
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			sabirnickiRastavljacDPS2.uklop(sabirnickiRastavljacDPS1);
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			sabirnickiRastavljacDPS2.isklop(sabirnickiRastavljacDPS1);
+	
+		}
+		
+		if(e.getActionCommand() == "") {
+			
+			sabirnickiRastavljacSPS1.uklop(sabirnickiRastavljacSPS1);
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			sabirnickiRastavljacSPS1.isklop(sabirnickiRastavljacSPS1);
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			sabirnickiRastavljacSPS2.uklop(sabirnickiRastavljacSPS1);
+	
+		}
+
+		if(e.getActionCommand() == "") {
+	
+			sabirnickiRastavljacSPS2.isklop(sabirnickiRastavljacSPS1);
+	
+		}
 		
 	}
 	
